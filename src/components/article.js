02abd0e8717618689ec5e851fd5c247e8cd2379a
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-11 23:04:11 +0200

    Add article component

---
+++ src/components/article.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './article.css'
+
+export const Article = ({ title, info, by, image }) => (
+  <div className="article-container" >
+    <div className='article'>
+      <img src={image} />
+      <div className='detail'>
+        <p className='title'>{title}</p>
+        <p className='info'>{info}</p>
+        <p className='writer'>
+            <small> - {by.name}</small>
+            <span className="btn"> Read </span>
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export const ArticleGroup = ({ articles, title }) => (
+    <div className="article-group-container" >
+      <h5> Recent articles</h5>
+      <div className='articles'>
+        {articles.map(article => (
+            <Article {...article} />
+        ))}
+      </div>
+    </div>
+  )
+
+export default Article
